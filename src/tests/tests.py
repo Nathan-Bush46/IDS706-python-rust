a31aed2cf57c61b10bb6f0fc2ace0e38e2619f73
Nathan-Bush46 <nathanbush46@gmail.com>
2024-10-29 03:02:40 +0000

    Added simple test and .env for python path

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,31 +1,7 @@
-from make_table import load
-from query import query
-
-from dotenv import load_dotenv
-from databricks import sql
-import os
-
+from simple_add import sum_of_squares
 
 def test_make_table():
-    assert load() == 0  # function has testing built in
-
-
-def test_query():
-    assert query() == 0  # function is somewhat simple
-    load_dotenv()
-    with sql.connect(  # just test that extra table is made
-        server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
-        http_path=os.getenv("DATABRICKS_HTTP_PATH"),
-        access_token=os.getenv("DATABRICKS_TOKEN"),
-    ) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM AvgSleepDurationByOccupation")
-            result = cursor.fetchall()
-            assert (result) is not None
-            cursor.close()
-            connection.close()
-
-
-if __name__ == "__main__":
-    test_make_table()
-    test_query()
+    numbers = list(range(11))
+    # Timing the function
+    result = sum_of_squares(numbers)
+    assert result == 385    
